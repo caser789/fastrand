6f7a9ba20173439885ddc34aae697a5be185ffc1
Xue Jiao <jiao.xue@seamoney.com>
2021-10-08 10:52:07 +0800

    impo Uint32n

--- fastrand.go
+++ fastrand.go
@@ -1,9 +1,39 @@
+// Package fastrand implements fast pesudorandom number generator
+// that should scale well on multi-CPU systems.
+//
+// Use crypto/rand instead of this package for generating
+// cryptographically secure random numbers.
 package fastrand
 
 import (
 	"sync"
 	"time"
 )
+
+// Uint32 returns pseudorandom uint32.
+//
+// It is safe calling this function from concurrent goroutines.
+func Uint32() uint32 {
+	v := rngPool.Get()
+	if v == nil {
+		v = &RNG{}
+	}
+	r := v.(*RNG)
+	x := r.Uint32()
+	rngPool.Put(r)
+	return x
+}
+
+var rngPool sync.Pool
+
+// Uint32n returns pseudorandom uint32 in the range [0..maxN).
+//
+// It is safe calling this function from concurrent goroutines.
+func Uint32n(maxN uint32) uint32 {
+	x := Uint32()
+	// See http://lemire.me/blog/2016/06/27/a-fast-alternative-to-the-modulo-reduction/
+	return uint32((uint64(x) * uint64(maxN)) >> 32)
+}
 
 // RNG is a pseudorandom number generator.
 //
@@ -46,20 +76,4 @@ func (r *RNG) Seed(n uint32) {
 func getRandomUint32() uint32 {
 	x := time.Now().UnixNano()
 	return uint32((x >> 32) ^ x)
-}
-
-var rngPool sync.Pool
-
-// Uint32 returns pseudorandom uint32.
-//
-// It is safe calling this function from concurrent goroutines.
-func Uint32() uint32 {
-	v := rngPool.Get()
-	if v == nil {
-		v = &RNG{}
-	}
-	r := v.(*RNG)
-	x := r.Uint32()
-	rngPool.Put(r)
-	return x
 }
--- fastrand_test.go
+++ fastrand_test.go
@@ -4,6 +4,32 @@ import (
 	"testing"
 )
 
+func TestRNGSeed(t *testing.T) {
+	var r RNG
+	for _, seed := range []uint32{1, 2, 1234, 432432, 34324432} {
+		r.Seed(seed)
+		m := make(map[uint32]struct{})
+		for i := 0; i < 1e6; i++ {
+			n := r.Uint32()
+			if _, ok := m[n]; ok {
+				t.Fatalf("number %v already exists", n)
+			}
+			m[n] = struct{}{}
+		}
+	}
+}
+
+func TestUint32(t *testing.T) {
+	m := make(map[uint32]struct{})
+	for i := 0; i < 1e6; i++ {
+		n := Uint32()
+		if _, ok := m[n]; ok {
+			t.Fatalf("number %v already exists", n)
+		}
+		m[n] = struct{}{}
+	}
+}
+
 func TestRNGUint32(t *testing.T) {
 	var r RNG
 	m := make(map[uint32]struct{})
@@ -16,17 +42,25 @@ func TestRNGUint32(t *testing.T) {
 	}
 }
 
-func TestRNGSeed(t *testing.T) {
-	var r RNG
-	for _, seed := range []uint32{1, 2, 1234, 432432, 34324432} {
-		r.Seed(seed)
-		m := make(map[uint32]struct{})
-		for i := 0; i < 1e6; i++ {
-			n := r.Uint32()
-			if _, ok := m[n]; ok {
-				t.Fatalf("number %v already exists", n)
-			}
-			m[n] = struct{}{}
+func TestUint32n(t *testing.T) {
+	m := make(map[uint32]int)
+	for i := 0; i < 1e6; i++ {
+		n := Uint32n(1e2)
+		if n >= 1e2 {
+			t.Fatalf("n > 1000: %v", n)
+		}
+		m[n]++
+	}
+
+	// check distribution
+	avg := 1e6 / 1e2
+	for k, v := range m {
+		p := (float64(v) - float64(avg)) / float64(avg)
+		if p < 0 {
+			p = -p
+		}
+		if p > 0.05 {
+			t.Fatalf("skew more than 5%% for k=%v: %v", k, p*100)
 		}
 	}
 }
@@ -52,16 +86,5 @@ func TestRNGUint32n(t *testing.T) {
 		if p > 0.05 {
 			t.Fatalf("skew more than 5%% for k=%v: %v", k, p*100)
 		}
-	}
-}
-
-func TestUint32(t *testing.T) {
-	m := make(map[uint32]struct{})
-	for i := 0; i < 1e6; i++ {
-		n := Uint32()
-		if _, ok := m[n]; ok {
-			t.Fatalf("number %v already exists", n)
-		}
-		m[n] = struct{}{}
 	}
 }
